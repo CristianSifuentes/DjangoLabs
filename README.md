51039dae19bcd44425ab5654b409dff0555a8dfd
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-07-27 16:51:49 -0500

    20200727: We have known aboout how we can use foreingkey and ManyToManyFields in django

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ This is a repository for learn about python framework called django
 django-admin startproject mysite
 python manage.py createsuperuser
 python manage.py runserver
+python -m django --version
+py -m pip install Django
+pip install psycopg2
+pip freeze django
+python manage.py makemigrations
--- mysite/pet/models.py
+++ mysite/pet/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
+from adoption.models import Person
+
+
+class Vaccine(models.Model):
+    name = models.CharField(max_length=50)
+    
+
+
 class Pet(models.Model):
     folio = models.CharField(max_length=10, primary_key=True)
     name = models.CharField(max_length=50)
     gender = models.CharField(max_length=10)
     age = models.IntegerField()
     redemption_date = models.DateField()
+    person = models.ForeignKey(Person, null=True, blank=True, on_delete=models.CASCADE)
+    vaccine = models.ManyToManyField(Vaccine)
