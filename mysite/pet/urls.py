88d68b22db8e57834b307ccee3bb02d9a3229b0b
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-08-11 19:31:21 -0500

    20200811: We implemented a new url for list new information

--- mysite/pet/urls.py
+++ mysite/pet/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from pet.views import index, pet_view
+from pet.views import index, pet_view, pet_list
 
 urlpatterns = [
     path('', index, name='index'),
-    path('add/', pet_view, name='pet_view')
+    path('add/', pet_view, name='pet_view'),
+    path('list/', pet_list, name='pet_list')
 
 ]
--- mysite/pet/views.py
+++ mysite/pet/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from pet.forms import PetForm
+from pet.models import Pet
 
 # Create your views here.
 def index(request):
@@ -10,9 +11,14 @@ def pet_view(request):
     if request.method == 'POST':
         form = PetForm(request.POST)
         if form.is_valid():
-            form.save(commit=False)
-        return redirect('pet:index')
+            form.save()
+        return redirect('index')
     else:
         form = PetForm()
 
     return render(request, 'pet/pet_form.html', { 'form': form })
+
+def pet_list(request):
+    pet = Pet.objects.all()
+    context = {'pet': pet}
+    return render(request, 'pet/pet_list.html', context)
