e42a887f9d96862c268bae49f8bda7b57748b551
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-08-25 18:20:01 -0500

    20200825: We have modified update request adoption for manage a better way

--- mysite/adoption/views.py
+++ mysite/adoption/views.py
@@ -44,15 +44,41 @@ class AdoptionRequestCreate(CreateView):
             return self.render_to_response(self.get_context_data(form=form, form2=form2))
 
 class AdoptionRequestUpdate(UpdateView):
-     model = AdoptionRequest
-     form_class = AdoptionRequestForm
-     template_name = 'adoption/adoptionrequest_form.html'
+    model = AdoptionRequest
+    second_model = Person
+    template_name = 'adoption/adoptionrequest_form.html'
+    form_class = AdoptionRequestForm
+    second_form_class = PersonForm
 
-     def get_absolute_url(self):
-         return reverse("adoptionrequest:adoptionrequest_form", kwargs={"pk": self.pk})
-    
-     def get_success_url(self):
-         return reverse('adoptionrequest_list')
+    def get_context_data(self, **kwargs):
+        context = super(AdoptionRequestUpdate, self).get_context_data(**kwargs)
+        pk = self.kwargs.get('pk', 0)
+        req = self.model.objects.get(id=pk)
+        person = self.second_model.objects.get(id=req.person_id)
+        if 'form' not in context:
+            context['form'] = self.form_class()
+        if 'form2' not in context:
+            context['form2'] = self.second_form_class(instance=person)
+        context['id'] = pk
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object
+        id_req = kwargs['pk']
+        req = self.model.objects.get(id=id_req)
+        person = self.second_model.objects.get(id=req.person_id)
+        form = self.form_class(request.POST, instance=req)
+        form2 = self.second_form_class(request.POST, instance=person)
+        if form.is_valid() and form2.is_valid():
+            form.save()
+            form2.save()
+            return HttpResponseRedirect(self.get_success_url())
+        else:
+            return HttpResponseRedirect(self.get_success_url())
+        
+
+    def get_success_url(self):
+          return reverse('adoptionrequest_list')
     
     
 class AdoptionRequestDelete(DeleteView):
